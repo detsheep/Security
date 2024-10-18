864b54d41707af47dcd50289d046cbf90ccb5c63
detsheep <96671101+detsheep@users.noreply.github.com>
2024-10-18 12:39:46 +0800

    Update index.php

--- g9/index.php
+++ g9/index.php
@@ -4,7 +4,7 @@ include "header.php";
 <body>
     <div style="margin-left:270px;margin-top:-650px">
     <p>
-    <strong><span style="font-size: 40px;">小组成员：<hr>赵艺阳，李哲豪，颜凡，郑孝维，钱一铭</span></strong>    
+    <strong><span style="font-size: 40px;">number：<hr>张三、李四、王五、赵六、老八</span></strong>    
     </p>
     </div>
 </body>
